542a31cc0c65d9c95eb9f2742123937eddac8b4d
kovacsv <viktorkovacs@gmail.com>
2021-09-20 18:57:05 +0200

    Basic material import.

--- source/import/importerassimp.js
+++ source/import/importerassimp.js
@@ -48,8 +48,7 @@ OV.ImporterAssimp = class extends OV.ImporterBase
 	{
 		const fileBuffers = this.callbacks.getAllBuffers ();
         let fileList = new this.assimpjs.FileList ();
-        for (let i = 0; i < fileBuffers.length; i++) {
-            const fileBuffer = fileBuffers[i];
+        for (const fileBuffer of fileBuffers) {
             fileList.AddFile (fileBuffer.name, new Uint8Array (fileBuffer.content));
         }
 
@@ -66,15 +65,16 @@ OV.ImporterAssimp = class extends OV.ImporterBase
     {
         function EnumerateNode (node, processor)
         {
-            // TODO: recursive
+            processor (node);
             if (node.children === undefined) {
                 return;
             }
-            for (let i = 0; i < node.children.length; i++) {
-                const child = node.children[i];
-                processor (child);
+            for (const child of node.children) {
+                EnumerateNode (child, processor);
             }
         }
+
+        this.ImportJsonMaterials (assimpJson);
 
         const rootNode = assimpJson.rootnode;
         if (rootNode === undefined) {
@@ -90,13 +90,48 @@ OV.ImporterAssimp = class extends OV.ImporterBase
         });
     }
 
+    ImportJsonMaterials (assimpJson)
+    {
+        function JsonColorToColor (jsonColor)
+        {
+            return new OV.Color (
+                parseInt (jsonColor[0] * 255.0, 10),
+                parseInt (jsonColor[1] * 255.0, 10),
+                parseInt (jsonColor[2] * 255.0, 10)
+            );
+        }
+
+        if (assimpJson.materials === undefined) {
+            return;
+        }
+
+        for (const jsonMaterial of assimpJson.materials) {
+            let material = new OV.Material (OV.MaterialType.Phong);
+            // TODO: other properties
+            // TODO: textures
+            for (const jsonProperty of jsonMaterial.properties) {
+                if (jsonProperty.key === '?mat.name') {
+                    material.name = jsonProperty.value;
+                } else if (jsonProperty.key === '$clr.diffuse') {
+                    material.color = JsonColorToColor (jsonProperty.value);
+                } else if (jsonProperty.key === '$clr.ambient') {
+                    material.ambient = JsonColorToColor (jsonProperty.value);
+                } else if (jsonProperty.key === '$clr.specular') {
+                    material.specular = JsonColorToColor (jsonProperty.value);
+                }
+            }
+            this.model.AddMaterial (material);
+        }
+    }
+
     ImportJsonMeshes (assimpJson, meshIndices)
     {
-        for (let i = 0; i < meshIndices.length; i++) {
-            let jsonMesh = assimpJson.meshes[meshIndices[i]];
+        for (const meshIndex of meshIndices) {
+            let jsonMesh = assimpJson.meshes[meshIndex];
             if (jsonMesh.vertices === undefined || jsonMesh.faces === undefined) {
                 continue;
             }
+
             let mesh = new OV.Mesh ();
             for (let i = 0; i < jsonMesh.vertices.length; i += 3) {
                 let vertex = new OV.Coord3D (
@@ -106,6 +141,12 @@ OV.ImporterAssimp = class extends OV.ImporterBase
                 );
                 mesh.AddVertex (vertex);
             }
+
+            let materialIndex = null;
+            if (jsonMesh.materialindex !== undefined) {
+                materialIndex = jsonMesh.materialindex;
+            }
+
             for (let i = 0; i < jsonMesh.faces.length; i++) {
                 let jsonFace = jsonMesh.faces[i];
                 let triangle = new OV.Triangle (
@@ -113,8 +154,10 @@ OV.ImporterAssimp = class extends OV.ImporterBase
                     jsonFace[1],
                     jsonFace[2]                    
                 );
+                triangle.SetMaterial (materialIndex);
                 mesh.AddTriangle (triangle);
             }
+            
             this.model.AddMesh (mesh);
         }
     }
